7a3dc05774a4fcd7af4d499d2b45d7c0b93b353b
Gavh2909 <satishgavhane2909@gmail.com>
2023-12-11 16:01:50 +0530

    App created and registration page created and its receiving data

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import UserRegister from "./Components/Pages/UserRegister";
+
+function App() {
+  return (
+    <div className="App">
+      <UserRegister />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Pages/UserRegister.js
@@ -0,0 +1,100 @@
+import "../Styles/UserRegister.css";
+import { useState } from "react";
+
+function UserRegister() {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    mobile: "",
+    state: "",
+    district: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    try {
+      console.log(formData);
+    } catch (error) {
+      console.log("Error Registering");
+    }
+  };
+  return (
+    <div className="register-home">
+      <h2>Registration</h2>
+
+      <input
+        type="text"
+        value={formData.firstName}
+        name="firstName"
+        onChange={handleChange}
+        placeholder="First Name"
+      />
+
+      <input
+        type="text"
+        value={formData.lastName}
+        name="lastName"
+        onChange={handleChange}
+        placeholder="Last Name"
+      />
+
+      <input
+        type="text"
+        value={formData.email}
+        name="email"
+        onChange={handleChange}
+        placeholder="Email"
+      />
+
+      <input
+        type="text"
+        value={formData.mobile}
+        name="mobile"
+        onChange={handleChange}
+        placeholder="Mobile"
+      />
+
+      <input
+        type="text"
+        value={formData.state}
+        name="state"
+        onChange={handleChange}
+        placeholder="State"
+      />
+
+      <input
+        type="text"
+        value={formData.district}
+        name="district"
+        onChange={handleChange}
+        placeholder="District"
+      />
+
+      <input
+        type="password"
+        value={formData.password}
+        name="password"
+        onChange={handleChange}
+        placeholder="Password"
+      />
+
+      <input type="password" value="" placeholder="Confirn Password" />
+
+      <button onClick={handleSubmit}>Submit</button>
+
+      <label>
+        <a href="user/pwdreset">forget password?</a>
+        <a href="user/login">Login</a>
+      </label>
+    </div>
+  );
+}
+
+export default UserRegister;
