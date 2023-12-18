19bf420f2348dd5a39c547e572debdc5c3a2afcc
Satish Gavhane <satishgavhane2909@gmail.com>
2023-12-18 22:27:51 +0530

    AD

--- src/Components/Pages/Logged/LandingPage.js
+++ src/Components/Pages/Logged/LandingPage.js
@@ -3,25 +3,30 @@ import LoggedHome from "./LoggedHome";
 import VehicleRegister from "./VehicleRegister";
 import ProfilePage from "./ProfilePage";
 import Vehicles from "./Vehicles";
+import {loginFailure} from "../../../redux/actions/userActions"
+import "../../Styles/LandingPage.css";
+import { connect } from "react-redux";
+
 
 function LandingPage({ dispatchLoginFailure }) {
   const [click, setClick] = useState("home");
   return (
     <div className="landing-page">
-
-      <a onClick={() => setClick("new-veh")}>Vehicle</a>
-      <a onClick={() => setClick("new-ent")}>New Entry</a>
+    <div className="navbar-log">
       <a onClick={() => setClick("home")}>Home</a>
-      <a onClick={() => setClick("profile")}>Profile</a>
-      {/* <a onClick={() => setClick("profile")}>LogOut</a> Implement this */}
-
-      {click === "home" && <LoggedHome />}
-      {click === "new-veh" && <Vehicles />}
-      {click === "profile" && <ProfilePage />}
-      {click === "new-ent" && <h1>This is Add new Entry page</h1>}
-
-      <h1>THis is Footer</h1>
+      <nav>
+        <a onClick={() => setClick("new-veh")}>Vehicles</a>
+        <a onClick={() => setClick("new-ent")}>Entries</a>
+        <a onClick={() => setClick("profile")}>Profile</a>
+        <a onClick={() => setClick("log-out")}>Log Out</a>
+      </nav>
     </div>
+
+    {click === "home" && <h1>Home</h1>}
+    {click === "new-veh" && <h1>Vehicles</h1>}
+    {click === "new-ent" && <h1>Entry</h1>}
+    {click === "profile" && <h1>Profile</h1>}
+  </div>
   );
 }
 
